b176ed3511e98a97219a1c9d5dd777429d6a1126
Anthony A. Russo <aarusso.exe@outlook.com>
2025-07-27 19:01:30 -0500

    Restructured the PlannedExpense and PlannedIncome classes to share a common base class, _PlannedItem.

--- src/budget/__init__.py
+++ src/budget/__init__.py
@@ -1,46 +1,79 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import Literal
+from typing import Literal, Any
 from currencies import Currency  # type: ignore
 
 from .enums import ExpenseCategory, ExpensePriority, IncomeCategory
+
+type CalendarTimeUnits = Literal[
+    "months", "years"
+]  # Literal["days", "weeks", "months", "years"]
 
 
 @dataclass
 class CalendarTime:
     value: int
-    unit: Literal["days", "weeks", "months", "years"]
+    unit: CalendarTimeUnits
 
     def __post_init__(self):
         if self.value <= 0:
             raise ValueError("Calendar time must be at least one!")
 
 
-@dataclass
-class SavingsContribution:
-    savings_goal: str
+@dataclass(kw_only=True)
+class _PlannedItem:
+    name: str
+    category: Any
+    currency: Currency
+    amount: float
+    recurrence: CalendarTime | None = None
+
+    def get_normalized_monthly_amount(self) -> float:
+        if self.recurrence is None:
+            return self.amount
+        else:
+            if self.recurrence.unit == "years":
+                return self.amount / (12 * self.recurrence.value)
+            elif self.recurrence.unit == "months":
+                return self.amount / (self.recurrence.value)
+            else:
+                raise TypeError(
+                    "A CalendarTime's unit attribute was set to an unsupported value! "
+                    "Please only use 'months' or 'years' for this field."
+                )
+
+    def __new__(cls, *args, **kwargs):
+        if cls is _PlannedItem:
+            raise TypeError(
+                "Cannot instantiate the class _PlannedItem directly! "
+                "Please use PlannedExpense or PlannedIncome instead."
+            )
+        return super().__new__(cls, *args, **kwargs)
 
 
 @dataclass(kw_only=True)
-class PlannedExpense:
+class PlannedExpense(_PlannedItem):
     """Always used to express planned expenses (or savings contributions), not income."""
 
-    name: str
-    category: ExpenseCategory | SavingsContribution
+    category: ExpenseCategory
     priority: ExpensePriority
-    currency: Currency
-    amount: float
-    tax: float
     recurrence: CalendarTime | None = None
+    # used to indicate that the "expense" is a savings contribution towards a particular goal
+    savings_goal: str | None = None
 
 
 @dataclass(kw_only=True)
-class PlannedIncome:
-    name: str
+class PlannedIncome(_PlannedItem):
     category: IncomeCategory
-    currency: Currency
-    amount: float
     post_tax: bool
-    recurrence: CalendarTime | None = None
+
+
+@dataclass(kw_only=True)
+class SavingsGoal:
+    name: str
+    timeline: CalendarTime
+    amount: float
 
 
 class Budget:
@@ -48,4 +81,32 @@ class Budget:
     the realization of actual financial events."""
 
     def __init__(self, *entries: PlannedExpense | PlannedIncome) -> None:
+        self.balance: float = 0.0
         self.entries: list[PlannedExpense | PlannedIncome] = list(entries)
+        self.saving_goals: dict[str, SavingsGoal] = {}
+        self.allocations: dict[ExpenseCategory, float] = {
+            k: 0.0 for k in ExpenseCategory
+        }
+
+        for entry in entries:
+            if not (
+                isinstance(entry, PlannedExpense) or isinstance(entry, PlannedIncome)
+            ):
+                raise TypeError(
+                    "Entries must be either a PlannedExpense or PlannedIncome object!"
+                )
+            if isinstance(entry, PlannedExpense):
+                self.allocations[entry.category] += entry.amount
+
+    def get_monthly_total(self) -> float:
+        """Returns the sum of all income minus the sum of all expenses on a monthly basis. All
+        annual items are divided by 12 for this consideration. All multi-month items are divided
+        by the number of months they span.
+
+        TODO: implement a strategy for dealing with daily & weekly items."""
+        total = 0.0
+        for entry in self.entries:
+            sign = -1.0 if isinstance(entry, PlannedExpense) else 1.0
+            total += sign * entry.get_normalized_monthly_amount()
+
+        return total
